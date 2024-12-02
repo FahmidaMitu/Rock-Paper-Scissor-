46e80c2b332fc66bfa55a8e5ad635c85cf25172b
FahmidaMitu <fahmidamitu57@gmail.com>
2024-12-02 18:08:34 +0600

    Complete

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-//Get Buttons
-const rockBtn = document.getElementById("rock");
-const paperBtn = document.getElementById("paper");
-const scissorBtn = document.getElementById("scissor");
-const winResult= document.getElementById("result");
 
-//Add Event listener
-rockBtn.addEventListener('click',() => playgame(0));
-paperBtn.addEventListener('click',() => playgame(1));
-scissorBtn.addEventListener('click',() => playgame(2));
+//Get input value
+const userChoice = parseInt(document.getElementById("userChoiceNum").value);
+
+//Take result in variable
+const showResult = document.getElementById("result");
+
+// Generate computer's random choice
+const computerChoice = Math.floor(Math.random() * 3);
+
+// Add event listener to the submit button
+document.getElementById("submit").addEventListener("click", function() {
+
+// Determine the result
+    if (userChoice === computerChoice) {
+        result = "It's a Tie!";
+    } else if (
+        (userChoice === 0 && computerChoice === 2) ||
+        (userChoice === 1 && computerChoice === 0) ||
+        (userChoice === 2 && computerChoice === 1)
+    ) {
+        result = "You Win!";
+    } else {
+        result = "You Lose!";
+    }
+// Display the result
+    showResult.innerHTML = `<p >${result}</p>`;
+});
 
 
 
